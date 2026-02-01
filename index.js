9b82f0a2f02f4f8f4b6af7f0e45adf58b36f7235
Vikranth Kumar Jakkoju <jakkojuvikranth@gmail.com>
2026-02-01 18:30:11 +0530

    Refactor name prompt to run on window load

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const name = prompt("Can I know your name?");
+var name;
+window.onload = () => {
+    name = prompt("Can I know your name?");
+};
+
+// const name = prompt("Can I know your name?");
 if(name) {
     var greet = "Greetings Mr / Ms " + name;
     document.querySelector("h3").innerText = greet;
