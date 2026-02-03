2923ccab56d108a196d4606a9c6041c4a31a1043
Vikranth Kumar Jakkoju <jakkojuvikranth@gmail.com>
2026-02-03 10:04:38 +0530

    Refactor name prompt to a constant declaration

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-var name;
-window.onload = () => {
-    name = prompt("Can I know your name?");
-};
-
-// const name = prompt("Can I know your name?");
+const name = prompt("Can I know your name?");
 if(name) {
     var greet = "Greetings Mr / Ms " + name;
     document.querySelector("h3").innerText = greet;
